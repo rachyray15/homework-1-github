7ee62caaf0da5c5ee6433144ae53038b06a97b2d
Rachel <raanchha@gmail.com>
2017-01-19 10:50:00 -0500

    Update SlicerScripts.py

--- SlicerScripts.py
+++ SlicerScripts.py
@@ -7,7 +7,7 @@ def handleButton():
     slicer.mrmlScene.AddNode(model)
     transformation = vtk.vtkTransform()
     transformation.PreMultiply()
-    transformation.Translate(300, 0, 0)
+    transformation.Translate(50, 0, 0)
     transformation.Update()
     model.SetAndObserveTransformToParent(transformation)
     node.SetAndObserveTransformNodeID(model.GetID())
